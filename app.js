6d26ee78a8d68e7edc4bd53f08432fe641c22c77
Grace Balanquit <gbalanquit622@gmail.com>
2020-07-23 16:41:47 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// // source online
+// // https://mikkegoes.com/javascript-projects-for-beginners/
+
+// // source code from codepen
+// // https://codepen.io/tutsplus/pen/QNeJgR
+
+// (function () {
+//   "use strict";
+
+//   // define variables
+//   var items = document.querySelectorAll(".timeline li");
+
+//   // check if an element is in viewport
+//   // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+
+//   function isElementInViewport(el) {
+//     var rect = el.getBoundingClientRect();
+//     return;
+//   }
+
+//   function callbackFunc() {
+//     for (var i = 0; i < items.length; i++) {
+//       if (isElementInViewport(items[i])) {
+//         items[i].classList.add("in-inview");
+//       }
+//     }
+//   }
+
+//   //  listen for events
+//   window.addEventListener("load", callbackFunc);
+//   window.addEventListener("resize", callbackFunc);
+//   window.addEventListener("scroll", callbackFunc);
+// })();
+
+(function () {
+  "use strict";
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
